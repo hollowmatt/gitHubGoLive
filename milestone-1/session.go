af109e7289c260a1d9a79291fb0d02714f9d3bad
Matt Holloway <hollowmatt@gmail.com>
2024-05-06 13:07:50 -0400

    work on session and cookie

--- milestone-1/session.go
+++ milestone-1/session.go
@@ -1,8 +1,23 @@
 package main
 
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
 type userData struct {
 	Login       string
 	accessToken string
 }
 
 var sessionStore = make(map[string]userData)
+
+func getRandomString() (string, error) {
+	c := 32
+	b := make([]byte, c)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(b), nil
+}
