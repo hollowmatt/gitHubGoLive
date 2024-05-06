1db7ad8dd711e76380e540154275cd1700a59dc5
Matt Holloway <hollowmatt@gmail.com>
2024-05-05 20:26:39 -0400

    add http server and handlers for two url endpoints

--- milestone-1/main.go
+++ milestone-1/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+)
+
+var conf = &oauth2.Config{
+	ClientID:     os.Getenv("ghclient"),
+	ClientSecret: os.Getenv("ghsecret"),
+	Scopes:       []string{"repo", "user"},
+}
+
+func indexHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+}
+
+func githubCallbackHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello callback, %s!", request.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/github/callback", githubCallbackHandler)
+	http.ListenAndServe(":8080", nil)
+}
